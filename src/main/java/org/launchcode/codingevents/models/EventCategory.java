27dba637065bc3e7bd7c70720d2a9089dfdd29d8
Kavitha <kavithamsca@gmail.com>
2023-09-04 22:21:09 -0500

    done one-to-many

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,18 +3,19 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 public class EventCategory extends AbstractEntity {
 
-//    @Id
-//    @GeneratedValue
-//    private int id;
-
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -30,25 +31,13 @@ public class EventCategory extends AbstractEntity {
         this.name = name;
     }
 
-//    public int getId() {
-//        return id;
-//    }
+    public List<Event> getEvents() {
+        return events;
+    }
 
     @Override
     public String toString() {
         return name;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        EventCategory that = (EventCategory) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
 }
